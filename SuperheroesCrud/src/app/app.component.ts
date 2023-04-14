8b6b217c13fb68b546ec217fb9a077887df36f07
dang3rzone <andresrosalespinzon@gmail.com>
2023-04-14 14:54:13 +0200

    feat: better typescript use

--- SuperheroesCrud/src/app/app.component.ts
+++ SuperheroesCrud/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { DialogComponent } from './dialog/dialog.component';
-import { ApiService } from './services/api.service';
+import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { ApiService } from './services/api.service';
+import { DialogComponent } from './dialog/dialog.component';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +13,6 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class AppComponent implements OnInit {
   title = 'SuperheroesCrud';
-
   displayedColumns: string[] = [
     'name',
     'category',
@@ -27,26 +26,26 @@ export class AppComponent implements OnInit {
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+
   constructor(private dialog: MatDialog, private api: ApiService) {}
 
   ngOnInit(): void {
     this.getAllSupes();
   }
 
-  openDialog() {
-    this.dialog
-      .open(DialogComponent, {
-        width: '30%',
-      })
-      .afterClosed()
-      .subscribe((data) => {
-        if (data === 'save') {
-          this.getAllSupes();
-        }
-      });
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '30%',
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === 'save') {
+        this.getAllSupes();
+      }
+    });
   }
 
-  getAllSupes() {
+  getAllSupes(): void {
     this.api.getSupe().subscribe({
       next: (res: any) => {
         this.dataSource = new MatTableDataSource(res);
@@ -54,38 +53,39 @@ export class AppComponent implements OnInit {
         this.dataSource.sort = this.sort;
       },
       error: (err: any) => {
-        alert('error while getting supes');
+        console.error('Error while getting superheroes:', err);
+        alert('Error while getting superheroes');
       },
     });
   }
 
-  editSupe(row: any) {
-    this.dialog
-      .open(DialogComponent, {
-        width: '30%',
-        data: row,
-      })
-      .afterClosed()
-      .subscribe((data) => {
-        if (data === 'update') {
-          this.getAllSupes();
-        }
-      });
+  editSupe(row: any): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: '30%',
+      data: row,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result === 'update') {
+        this.getAllSupes();
+      }
+    });
   }
 
-  deleteSupe(id: number) {
+  deleteSupe(id: number): void {
     this.api.deleteSupe(id).subscribe({
-      next: (res) => {
-        alert('successfully deleted supe');
+      next: () => {
+        alert('Successfully deleted superhero');
         this.getAllSupes();
       },
-      error: (res) => {
-        alert('error while deleting supe');
+      error: (err: any) => {
+        console.error('Error while deleting superhero:', err);
+        alert('Error while deleting superhero');
       },
     });
   }
 
-  applyFilter(event: Event) {
+  applyFilter(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
 
